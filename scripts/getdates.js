cee3919ccda4e6f5ca25cb08044d3bd8000339a8
ssaliarafat <marvelarafat@gmail.com>
2025-11-01 17:23:45 +0300

    My Landing Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+//Inserting the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//Displaying the last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
